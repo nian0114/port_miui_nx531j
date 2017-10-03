90410860b5458638793957cd41049f8961b9e818
Nian <268078545@qq.com>
2017-10-03 09:31:20 -0500

    fix libgcc_s.so error
    libgcc_s.so.1: cannot open shared object file: No such file or directory

--- centos.sh
+++ centos.sh
@@ -1,1 +1,1 @@
-yum install gcc zip unzip java git p7zip glibc.i686 zlib.i686 glibc-devel libstdc++*
+yum install gcc zip unzip java git p7zip glibc.i686 zlib.i686 glibc-devel libstdc++* libgcc_s.so.1
