4996e0252e52f965076b107e0ab2d7c01d33c62c
Nian <268078545@qq.com>
2017-10-03 12:47:31 -0500

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -1,1 +1,7 @@
+#add support for digitalocean
+cd /usr/local/src 
+wget -c http://mirrors.kernel.org/fedora-epel/epel-release-latest-7.noarch.rpm 
+rpm -ivh epel-release-latest-7.noarch.rpm 
+cd
+
 yum install gcc zip unzip java git p7zip glibc.i686 zlib.i686 glibc-devel libstdc++* libgcc_s.so.1
