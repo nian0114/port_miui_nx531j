1a6ba957b6bfab7464514575b9eb09ad11f275eb
Nian <268078545@qq.com>
2017-10-05 11:29:03 -0500

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -11,10 +11,10 @@ mv miui-$DEVICE-$Type-$VERSION-7.0.zip /var/www/html/
 #upload to qiniu
 ./tools/upload/qiniu/qshell_linux_amd64 rput ttotoo-addons-south miui-$DEVICE-$Type-$VERSION-7.0.zip /var/www/html/miui-$DEVICE-$Type-$VERSION-7.0.zip
 
+#upload to mega
+megamkdir /Root/MIUI9/$VERSION
+megaput --path /Root/MIUI9/$VERSION/miui-$DEVICE-$Type-$VERSION-7.0.zip /var/www/html/miui-$DEVICE-$Type-$VERSION-7.0.zip
+
 #upload to baidu
 bypy mkdir MIUI9/$VERSION
 bypy upload /var/www/html/miui-$DEVICE-$Type-$VERSION-7.0.zip MIUI9/$VERSION/
-
-#upload to mega
-./tools/upload/mega/megamkdir /Root/MIUI9/$VERSION
-./tools/upload/mega/megaput --path /Root/MIUI9/$VERSION/miui-$DEVICE-$Type-$VERSION-7.0.zip /var/www/html/miui-$DEVICE-$Type-$VERSION-7.0.zip
