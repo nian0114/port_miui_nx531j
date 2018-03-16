8076c67d92f2d5e832590bea98e75ec65d4036e6
nian0114 <268078545@qq.com>
2018-03-16 11:32:05 +0800

    use cosfs by source

--- cos.sh
+++ cos.sh
@@ -1,7 +1,11 @@
-wget https://github.com/tencentyun/cosfs/releases/download/v1.0.2/cosfs_1.0.2-ubuntu14.04_amd64.deb
-sudo dpkg -i cosfs_1.0.2-ubuntu14.04_amd64.deb
+git clone https://github.com/nian0114/cosfs.git
+cd cosfs
+./autogen.sh
+./configure
+make
+sudo make install
 
-echo ${bucket}:${access-key-id}:${access-key-secret} > /etc/passwd-cosfs
-chmod 640 /etc/passwd-cosfs
+echo ${bucket}:${access-key-id}:${access-key-secret} > /home/travis/passwd-cosfs
+chmod 640 /home/travis/passwd-cosfs
 mkdir /tmp/cosfs
 cosfs ${appid}:${bucket} /tmp/cosfs -ourl=http://cn-east.myqcloud.com -odbglevel=info
